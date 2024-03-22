35a19612c1e7e11b2c82ec4fcd836d6c762a9568
Kamrul Jaman <kamrul.jaman26@gmail.com>
2024-03-22 20:33:14 +0600

    added intregation testing with docker container

--- product-service/src/test/java/com/shop/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/shop/product/ProductServiceApplicationTests.java
@@ -1,13 +1,89 @@
 package com.shop.product;
 
+import com.shop.product.dto.ProductRequest;
+import com.shop.product.repository.ProductRepo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static MongoDBContainer container = new MongoDBContainer("mongo:4.4.2");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public ObjectMapper objectMapper() {
+            return new ObjectMapper();
+        }
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongo.uri", container::getReplicaSetUrl);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testCreateProduct() throws Exception {
+        getProductRequest();
+        String productJSON = objectMapper.writeValueAsString(getProductRequest());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productJSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void testGetProduct() throws Exception {
+        getProductRequest();
+        String productJSON = objectMapper.writeValueAsString(getProductRequest());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productJSON))
+                .andExpect(status().isOk());
+        Assertions.assertTrue(productRepo.findAll().size() != 0);
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("iphone 12")
+                .description("iphone 12")
+                .Price(123.0)
+                .build();
+    }
 
 }
